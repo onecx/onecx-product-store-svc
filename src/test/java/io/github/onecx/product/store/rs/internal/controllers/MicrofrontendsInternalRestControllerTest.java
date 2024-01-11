6ae9b4e9d716e079b849ee8a6a5a7815fdf01cd5
CSommer <christoph.sommer@capgemini.com>
2024-01-11 15:05:58 +0100

    fix: fixing deleting MFEs including uiendpoints and constraints, JUnit tests: scenarios for creation

--- src/test/java/io/github/onecx/product/store/rs/internal/controllers/MicrofrontendsInternalRestControllerTest.java
+++ src/test/java/io/github/onecx/product/store/rs/internal/controllers/MicrofrontendsInternalRestControllerTest.java
@@ -6,6 +6,8 @@ import static jakarta.ws.rs.core.Response.Status.*;
 import static jakarta.ws.rs.core.Response.Status.OK;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.tkit.quarkus.test.WithDBData;
@@ -20,57 +22,192 @@ import io.quarkus.test.junit.QuarkusTest;
 @WithDBData(value = "data/test-internal.xml", deleteBeforeInsert = true, deleteAfterTest = true, rinseAndRepeat = true)
 class MicrofrontendsInternalRestControllerTest extends AbstractTest {
 
+    /**
+     * Scenario: Create new microfrontend (mfe) in case product name and app-id path are unique (not yet existing). Full
+     * comparison of full defined mfe object.
+     * Given
+     * When I try to create a mfe with a non-existing (in backend) product name and app-id
+     * Then I get a 'OK' response code back
+     * AND created mfe is returned
+     */
     @Test
-    void createMicrofrontendTest() {
+    void createMicrofrontend_shouldAddNewMicrofrontend_whenProductnameAndAppIdAreUnique() {
+        Set<String> classificationSet = new HashSet<>();
+        classificationSet.add("Gaming");
+        classificationSet.add("Test");
+        List<CreateUIEndpointDTO> uiEndpointSetForRequest = new ArrayList<>();
+        CreateUIEndpointDTO uiEndpointItemForRequest = new CreateUIEndpointDTO();
+        uiEndpointItemForRequest.setName("Pages");
+        uiEndpointItemForRequest.setPath("/pages");
+        uiEndpointSetForRequest.add(uiEndpointItemForRequest);
+        CreateMicrofrontendRequestDTO request = createMicrofrontendRequest("App-ID-5", "1.0.0",
+                "AppName-5", "some description", "", "https://localhost/mfe/core/ah-mgmtv5/",
+                "https://localhost/mfe/core/ah-mgmtv5/remoteEntry.js", "ProductNamev5", classificationSet,
+                "developers@1000kit.org", "sun", "some notes", "/AnnouncementManagementModulev5", uiEndpointSetForRequest);
 
-        CreateMicrofrontendRequestDTO createDto = new CreateMicrofrontendRequestDTO();
-        createDto.setAppId("mfeId");
-        createDto.setAppVersion("0.0.0");
-        createDto.setExposedModule("exposed-module");
-        createDto.setRemoteBaseUrl("remote-base-url");
-        createDto.setRemoteEntry("remote-entry");
-        createDto.setAppName("display-name");
-        createDto.setProductName("product-name");
-        createDto.setRemoteBaseUrl("remote-base-url");
-
-        var dto = given()
+        // create
+        var responseCreationRequest = given()
                 .when()
                 .contentType(APPLICATION_JSON)
-                .body(createDto)
+                .body(request)
                 .post()
                 .then()
                 .statusCode(CREATED.getStatusCode())
                 .extract()
                 .body().as(MicrofrontendDTO.class);
 
-        assertThat(dto).isNotNull();
-        assertThat(dto.getAppName()).isNotNull().isEqualTo(createDto.getAppName());
+        // try to fetch newly created object
+        var responseGetRequest = given()
+                .contentType(APPLICATION_JSON)
+                .pathParam("id", responseCreationRequest.getId())
+                .get("{id}")
+                .then().statusCode(OK.getStatusCode())
+                .contentType(APPLICATION_JSON)
+                .extract()
+                .body().as(MicrofrontendDTO.class);
 
-        // create theme without body
-        var exception = given()
+        assertThat(responseCreationRequest).isNotNull();
+        assertThat(responseGetRequest).isNotNull();
+        assertThat(responseGetRequest.getId()).isNotNull();
+        assertThat(responseGetRequest.getCreationDate()).isNotNull();
+        assertThat(responseGetRequest.getModificationDate()).isNotNull();
+
+        assertThat(responseGetRequest.getModificationCount()).isNotNull()
+                .isEqualTo(0);
+        assertThat(responseGetRequest.getAppId()).isNotNull()
+                .isEqualTo(responseCreationRequest.getAppId());
+        assertThat(responseGetRequest.getAppVersion()).isNotNull()
+                .isEqualTo(responseCreationRequest.getAppVersion());
+        assertThat(responseGetRequest.getAppName()).isNotNull()
+                .isEqualTo(responseCreationRequest.getAppName());
+        assertThat(responseGetRequest.getDescription()).isNotNull()
+                .isEqualTo(responseCreationRequest.getDescription());
+        assertThat(responseGetRequest.getTechnology()).isNotNull()
+                .isEqualTo(responseCreationRequest.getTechnology());
+        assertThat(responseGetRequest.getRemoteBaseUrl()).isNotNull()
+                .isEqualTo(responseCreationRequest.getRemoteBaseUrl());
+        assertThat(responseGetRequest.getRemoteEntry()).isNotNull()
+                .isEqualTo(responseCreationRequest.getRemoteEntry());
+        assertThat(responseGetRequest.getProductName()).isNotNull()
+                .isEqualTo(responseCreationRequest.getProductName());
+        assertThat(responseGetRequest.getContact()).isNotNull()
+                .isEqualTo(responseCreationRequest.getContact());
+        assertThat(responseGetRequest.getIconName()).isNotNull()
+                .isEqualTo(responseCreationRequest.getIconName());
+        assertThat(responseGetRequest.getNote()).isNotNull()
+                .isEqualTo(responseCreationRequest.getNote());
+        assertThat(responseGetRequest.getExposedModule()).isNotNull()
+                .isEqualTo(responseCreationRequest.getExposedModule());
+
+        //classifications
+        assertThat(responseGetRequest.getClassifications().size())
+                .isEqualTo(responseCreationRequest.getClassifications().size());
+        Object[] arrayItem = responseCreationRequest.getClassifications().toArray();
+        assertThat(responseGetRequest.getClassifications()).contains(arrayItem[0].toString());
+        assertThat(responseGetRequest.getClassifications()).contains(arrayItem[1].toString());
+
+        // endpoints
+        List<UIEndpointDTO> endpointsResponse = responseGetRequest.getEndpoints();
+        Optional<UIEndpointDTO> endpointsResponseItem = endpointsResponse.stream()
+                .filter(e -> e.getName().equals(request.getEndpoints().get(0).getName()))
+                .findFirst();
+        assertThat(endpointsResponseItem).isPresent();
+        assertThat(responseGetRequest.getEndpoints().get(0).getName()).isNotNull()
+                .isEqualTo(endpointsResponseItem.get().getName());
+        assertThat(responseGetRequest.getEndpoints().get(0).getPath()).isNotNull()
+                .isEqualTo(endpointsResponseItem.get().getPath());
+
+    }
+
+    /**
+     * Scenario: Throw 400 Bad Request exception when provided product name and product id are already used for another
+     * microfrontend.
+     * Given
+     * When I try to create a mfe with used app id and product name
+     * Then I get a 'Bad Request' response code back
+     * AND problem details are within the response body
+     */
+    @Test
+    void createMicrofrontend_shouldReturnBadRequest_whenViolatingValueUniqueness() {
+        CreateMicrofrontendRequestDTO request = createMicrofrontendRequest("mfeId", "1.0.0",
+                "AppName-5", "some description", "", "https://localhost/mfe/core/ah-mgmtv5/",
+                "https://localhost/mfe/core/ah-mgmtv5/remoteEntry.js", "ProductName", null,
+                "developers@1000kit.org", "sun", "some notes", "/AnnouncementManagementModulev5", null);
+
+        // first shot (success)
+        given()
                 .when()
                 .contentType(APPLICATION_JSON)
+                .body(request)
                 .post()
                 .then()
-                .statusCode(BAD_REQUEST.getStatusCode())
-                .extract().as(ProblemDetailResponseDTO.class);
+                .statusCode(CREATED.getStatusCode())
+                .extract()
+                .body().as(MicrofrontendDTO.class);
 
-        assertThat(exception.getErrorCode()).isEqualTo("CONSTRAINT_VIOLATIONS");
-        assertThat(exception.getDetail()).isEqualTo("createMicrofrontend.createMicrofrontendRequestDTO: must not be null");
-
-        // create theme with existing name
-        exception = given().when()
+        // second shot (violates unique constraint)
+        var responseCreationRequest = given()
+                .when()
                 .contentType(APPLICATION_JSON)
-                .body(createDto)
+                .body(request)
                 .post()
                 .then()
                 .statusCode(BAD_REQUEST.getStatusCode())
-                .extract().as(ProblemDetailResponseDTO.class);
+                .extract()
+                .body().as(ProblemDetailResponseDTO.class);
 
-        assertThat(exception.getErrorCode()).isEqualTo("PERSIST_ENTITY_FAILED");
-        assertThat(exception.getDetail()).isEqualTo(
-                "could not execute statement [ERROR: duplicate key value violates unique constraint 'ps_microfrontend_app_id'  Detail: Key (product_name, app_id)=(product-name, mfeId) already exists.]");
+        assertThat(responseCreationRequest).isNotNull();
+        List<ProblemDetailParamDTO> problemDetailsParams = responseCreationRequest.getParams();
+        Optional<ProblemDetailParamDTO> paramConstraint = problemDetailsParams.stream()
+                .filter(e -> e.getKey().equals("constraint"))
+                .findFirst();
+        Optional<ProblemDetailParamDTO> paramConstraintName = problemDetailsParams.stream()
+                .filter(e -> e.getKey().equals("constraintName"))
+                .findFirst();
 
+        assertThat(paramConstraint).isPresent();
+        assertThat(paramConstraintName).isPresent();
+        assertThat(responseCreationRequest.getErrorCode()).isEqualTo("PERSIST_ENTITY_FAILED");
+        assertThat(responseCreationRequest.getDetail()).isEqualTo(
+                "could not execute statement [ERROR: duplicate key value violates unique constraint 'ps_microfrontend_app_id'  Detail: Key (product_name, app_id)=(ProductName, mfeId) already exists.]");
+
+    }
+
+    /**
+     * Scenario: Throw 400 Bad Request when mandatory field(s) is/are missing.
+     * Given
+     * When I try to create a mfe without setting all mandatory fields
+     * Then I get a 'Bad Request' response code back
+     * AND problem details are within the response body
+     */
+    @Test
+    void createMicrofrontend_shouldReturnBadRequest_whenRunningIntoValidationConstraints() {
+        CreateMicrofrontendRequestDTO request = createMicrofrontendRequest(null, "1.0.0",
+                "AppName-5", "some description", "", "https://localhost/mfe/core/ah-mgmtv5/",
+                "https://localhost/mfe/core/ah-mgmtv5/remoteEntry.js", "ProductName", null,
+                "developers@1000kit.org", "sun", "some notes", "/AnnouncementManagementModulev5", null);
+
+        var response = given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(request)
+                .post()
+                .then()
+                .statusCode(BAD_REQUEST.getStatusCode())
+                .extract()
+                .body().as(ProblemDetailResponseDTO.class);
+
+        List<ProblemDetailInvalidParamDTO> problemDetailsParams = response.getInvalidParams();
+        Optional<ProblemDetailInvalidParamDTO> invalidParamConstraint = problemDetailsParams.stream().findFirst();
+
+        assertThat(response).isNotNull();
+        assertThat(invalidParamConstraint).isPresent();
+
+        assertThat(response.getErrorCode()).isEqualTo("CONSTRAINT_VIOLATIONS");
+        assertThat(response.getDetail()).isEqualTo("createMicrofrontend.createMicrofrontendRequestDTO.appId: must not be null");
+        assertThat(invalidParamConstraint.get().getMessage()).isEqualTo("must not be null");
+        assertThat(invalidParamConstraint.get().getName()).isEqualTo("createMicrofrontend.createMicrofrontendRequestDTO.appId");
+        assertThat(response.getParams()).isNull();
     }
 
     @Test
@@ -254,4 +391,59 @@ class MicrofrontendsInternalRestControllerTest extends AbstractTest {
         Assertions.assertNotNull(exception.getInvalidParams());
         Assertions.assertEquals(1, exception.getInvalidParams().size());
     }
+
+    /**
+     * Helper method to create a microfrontend (mfe) request object (inbound call).
+     *
+     * @param appId unique id of application
+     * @param appVersion version number
+     * @param appName unique name of application
+     * @param description general mfe description
+     * @param technology representing used technology (angular, react, ...)
+     * @param remoteBaseUrl uri for remote base path
+     * @param remoteEntry webpack hook
+     * @param productName name of associated product
+     * @param classifications tags for mfe
+     * @param contact contact details (like mail address) for e.g. application support
+     * @param iconName identifier of PrimeNG icon lib, z.b. trash, times
+     * @param note additional notes
+     * @param exposedModule module information
+     * @param endpoints endpoints which can be used to address app pages & services
+     * @return
+     */
+    private CreateMicrofrontendRequestDTO createMicrofrontendRequest(
+            String appId,
+            String appVersion,
+            String appName,
+            String description,
+            String technology,
+            String remoteBaseUrl,
+            String remoteEntry,
+            String productName,
+            Set<String> classifications,
+            String contact,
+            String iconName,
+            String note,
+            String exposedModule,
+            List<CreateUIEndpointDTO> endpoints) {
+
+        CreateMicrofrontendRequestDTO mfe = new CreateMicrofrontendRequestDTO();
+        mfe.setAppId(appId);
+        mfe.setAppVersion(appVersion);
+        mfe.setAppName(appName);
+        mfe.setDescription(description);
+        mfe.setTechnology(technology);
+        mfe.setRemoteBaseUrl(remoteBaseUrl);
+        mfe.setRemoteEntry(remoteEntry);
+        mfe.setProductName(productName);
+        mfe.setClassifications(classifications);
+        mfe.setContact(contact);
+        mfe.setIconName(iconName);
+        mfe.setNote(note);
+        mfe.setExposedModule(exposedModule);
+        mfe.setEndpoints(endpoints);
+
+        return mfe;
+    }
+
 }
