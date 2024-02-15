74c48e1f4f7b02f43c15cde6fd954688c6ecaf58
Jannik Steenken <jannik.steenken@capgemini.com>
2024-02-15 14:16:38 +0100

    feat: setup images rest controller

---
+++ src/test/java/org/tkit/onecx/product/store/rs/internal/controllers/ImagesInternalRestControllerIT.java
@@ -0,0 +1,8 @@
+package org.tkit.onecx.product.store.rs.internal.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ImagesInternalRestControllerIT extends ImagesInternalRestControllerTest{
+
+}
