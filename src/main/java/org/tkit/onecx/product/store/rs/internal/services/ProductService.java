c1c5b5122956eb3bf56921e3b7d3860b7d5d7367
jsteenke <146953549+jsteenke@users.noreply.github.com>
2024-03-05 11:00:20 +0100

    feat: delete image by product delete (#44)

--- src/main/java/org/tkit/onecx/product/store/rs/internal/services/ProductService.java
+++ src/main/java/org/tkit/onecx/product/store/rs/internal/services/ProductService.java
@@ -1,12 +1,16 @@
 package org.tkit.onecx.product.store.rs.internal.services;
 
+import java.util.List;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 
+import org.tkit.onecx.product.store.domain.daos.ImageDAO;
 import org.tkit.onecx.product.store.domain.daos.MicrofrontendDAO;
 import org.tkit.onecx.product.store.domain.daos.MicroserviceDAO;
 import org.tkit.onecx.product.store.domain.daos.ProductDAO;
+import org.tkit.onecx.product.store.domain.models.Microfrontend;
 import org.tkit.onecx.product.store.domain.models.Product;
 
 @ApplicationScoped
@@ -20,10 +24,28 @@ public class ProductService {
     @Inject
     ProductDAO productDAO;
 
+    @Inject
+    ImageDAO imageDAO;
+
     @Transactional
     public void updateProductAndRelatedMfeAndMs(String oldProductName, Product updateItem) {
         microfrontendDAO.updateByProductName(oldProductName, updateItem.getName());
         microserviceDAO.updateByProductName(oldProductName, updateItem.getName());
         productDAO.update(updateItem);
+    }
+
+    @Transactional
+    public void deleteProduct(String id) {
+        var product = productDAO.findById(id);
+        if (product != null) {
+            List<Microfrontend> productRelatedMfes = microfrontendDAO.loadByProductName(product.getName()).toList();
+            microfrontendDAO.delete(productRelatedMfes);
+            microserviceDAO.deleteByProductName(product.getName());
+            productDAO.deleteQueryById(id);
+
+            // workaround for images
+            imageDAO.deleteQueryByRefId(product.getName());
+        }
+
     }
 }
