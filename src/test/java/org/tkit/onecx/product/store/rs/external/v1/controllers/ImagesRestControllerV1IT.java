2d12db095b37488f38489c2425b3fee781797198
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-05-14 11:38:58 +0200

    feat: added v1 getImage endpoint

---
+++ src/test/java/org/tkit/onecx/product/store/rs/external/v1/controllers/ImagesRestControllerV1IT.java
@@ -0,0 +1,7 @@
+package org.tkit.onecx.product.store.rs.external.v1.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ImagesRestControllerV1IT extends ImagesRestControllerV1Test {
+}
