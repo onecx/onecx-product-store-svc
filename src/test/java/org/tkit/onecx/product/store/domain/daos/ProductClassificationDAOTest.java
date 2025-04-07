699151197a0e94af40bf17fe502a108c891e33c3
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-04-07 09:17:19 +0200

    fix: remove public modifier

--- src/test/java/org/tkit/onecx/product/store/domain/daos/ProductClassificationDAOTest.java
+++ src/test/java/org/tkit/onecx/product/store/domain/daos/ProductClassificationDAOTest.java
@@ -14,7 +14,7 @@ import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
-public class ProductClassificationDAOTest {
+class ProductClassificationDAOTest {
     @Inject
     ProductClassificationDAO dao;
 
